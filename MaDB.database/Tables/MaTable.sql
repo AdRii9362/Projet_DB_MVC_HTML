9926f0e27d361a6a59d8033aa5960f8af3931ba4
AdRii9362 <147837506+AdRii9362@users.noreply.github.com>
2023-11-27 01:31:25 +0100

    Ajoutez des fichiers projet.

---
+++ MaDB.database/Tables/MaTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[MaTable]
+(
+	[Id] INT NOT NULL IDENTITY, 
+    [Nom] NCHAR(50) NOT NULL, 
+    [Prenom] NCHAR(50) NOT NULL, 
+    [Annee] INT NOT NULL, 
+    CONSTRAINT [PK_MaTable] PRIMARY KEY ([Id]),
+)
